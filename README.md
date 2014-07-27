c3ee345691dfbf3871d2d2e265eb9bde86c18f69
Michael Lamanuzzi <mlamanuzzi@walmart.com>
2014-07-27 12:27:13 -0700

    Course project for getting and cleaning data

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 getdata-005-project
 ===================
+
+run_analysis.R:
+
+Utilizes the data included in the Human Activity Recognition Using Smartphones Data Set provided by the UCI Machine Learning Repository. 
+
+With these data, the script:
+
+1. Extracts the feature names (measurements collected) from the features.txt file
+2. Extracts the activity labels (walking, walking upstairs, etc.) from the activity_labels.txt file
+3. From the feature names, gets the column indexes that correspond to the data that we are after - the means and the standard deviations
+4. Reads the x_train data set and labels the data set with the appropriate feature names from step 1.
+5. Reads the subject training and subject test data and names the columns
+6. Reads the x and y test files and names x test with the feature names and y test with "labels"
+7. Gets the mean and SD columns using the indexes for the mean and SD columns collected in step 3.
+8. Reads the x train and y train data sets, labels the x train with feature names and y train with labels
+9. Reads subject test and train files, names subjects
+10. Repeat step 8 for test data sets
+11. Extract the mean and SD column names for the x test data set (should be the same as x train)
+12. Creates a new training data set using subjects from step 9, y test from step 8, and the subset of x test containing the means and SDs
+13. Repeat step 12 for the test data set
+14. Combine the data from steps 12, 1
+15. Create a tidy data set with the average of each activity for each subject for each feature
+16. Order the tidy data set
+17. Use the activity labels from step 2 to label the tidy data set
+18. Write the tidy data set to CSV file "human_activity_recognition_tiny.csv"
+
+
+
+combines the test and and training data sets, while extracting only the mean and S.D. for each of the measurements described in the features.txt file included in the data set. Appropriate labels are given to the data set, and a tidy data set in CSV format is created the contains the average of each variable for each activity and each subject.
+
---
+++ run_analysis.R
@@ -0,0 +1,53 @@
+run_analysis <- function() {
+    setwd("data/UCI HAR Dataset/")
+    
+    ## Read the feature names (measurements)
+    features <- read.table("features.txt")["V2"]
+    print (features)
+    
+    ## Read the activity labels - walking, walking upstairs, etc.
+    activityLabels <- read.table("activity_labels.txt")["V2"]
+    print (activityLabels)
+    
+    ## Get the indexes of the columns we are after - mean, and S.D.
+    indexesOfMeansAndStds <- grep("mean|std", features$V2) 
+    print (indexesOfMeansAndStds)
+    
+    xTrain <- read.table("train/X_train.txt")
+    ## label the xTrain data with the appropriate feature names
+    names(xTrain) <- features$V2
+
+    yTrain <- read.table("train/y_train.txt")
+    names(yTrain) <- "labels"
+    
+    subjectTrain <- read.table("train/subject_train.txt")
+    names(subjectTrain) <- "subjects"
+    
+    subjectTest <- read.table("test/subject_test.txt")
+    names(subjectTest) <- "subjects"
+    
+    xTest <- read.table("test/X_test.txt")
+    names(xTest) <- features$V2
+    yTest <- read.table("test/y_test.txt")
+    names(yTest) <- "labels"
+    
+    ## Extract the mean and SD for each measurement
+    meansAndSDColnames <- colnames(xTest)[indexesOfMeansAndStds]
+    testing <- cbind(subjectTest, yTest, subset(xTest, select=meansAndSDColnames))
+    training <- cbind(subjectTrain, yTrain, subset(xTrain, select=meansAndSDColnames))
+    
+    ## Merge the training and test sets to create one data set
+    combined<-rbind(testing, training)
+    
+    ## Create a second, independent tidy data set with the average of each variable for each activity and each subject
+    tidyDataSet <- aggregate(combined[,3:ncol(combined)], list(Subject=combined$subjects, Activity=combined$labels), mean)
+    tidyDataSet <- tidyDataSet[order(tidyDataSet$Subject),]
+    
+    ## Use descriptive activity names to name the activities in the data set
+    tidyDataSet$Activity <- activityLabels[tidyDataSet$Activity,]
+    
+    setwd("../../")
+    write.table(tidyDataSet, file="human_activity_recognition_tiny.csv", sep=",", row.names=FALSE)
+    
+}
+run_analysis()
